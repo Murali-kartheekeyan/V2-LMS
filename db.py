1bbace1a6449f6122e68b69680a4a4cdbcbb0408
Murali-kartheekeyan <88695955+Murali-kartheekeyan@users.noreply.github.com>
2025-09-19 11:33:22 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,12 @@
-import pymysql
+import psycopg2
 import os
 
 def get_db_connection():
     return pymysql.connect(
-        host=os.getenv('DB_HOST', 'sql12.freesqldatabase.com'),
-        user=os.getenv('DB_USER', 'sql12795106'),
-        password=os.getenv('DB_PASSWORD', '3kC8zmrVMn'),
-        database=os.getenv('DB_NAME', 'sql12795106'),  # <-- UPDATED a
+        host=os.getenv('DB_HOST', 'dpg-d35tb3ndiees738kb2i0-a.singapore-postgres.render.com'),
+        user=os.getenv('DB_USER', 'new_ekjw_user'),
+        password=os.getenv('DB_PASSWORD', 'iS15JOBw5cpgAZHHAraVGSZyREb9rv2d'),
+        database=os.getenv('DB_NAME', 'new_ekjw'),  
         cursorclass=pymysql.cursors.DictCursor
 
     )   
-
-
